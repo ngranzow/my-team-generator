3703ddc962f7104ecfcd0c0e01d358a15d62cfd8
ngranzow <nate.granzow@gmail.com>
2022-03-06 21:56:53 -0600

    fixed cannot find module error

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,5 +1,5 @@
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const Manager = require('../lib/Manager');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
 
 module.exports = generateHTML;
